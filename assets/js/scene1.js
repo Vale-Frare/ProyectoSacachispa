4bedcb093b1768e2e0082916ffbc72171c9ce60c
Vale-Frare <valentinfrare.escanormal@gmail.com>
2021-06-21 02:04:17 -0300

    lawea

--- assets/js/scene1.js
+++ assets/js/scene1.js
@@ -19,113 +19,111 @@ class Scene1 extends Juego {
         boss.target = levelsData[0].spawn
     }
     update(time, delta) {   
-        if (gameOver) {
-            return;
-        }
-
-        if (spaceKey.isDown) {
-            if (player.body.onFloor()) {
-                player.body.setVelocityY(-180);
+        if (!gameOver) {
+            if (spaceKey.isDown) {
+                if (player.body.onFloor()) {
+                    player.body.setVelocityY(-180);
+                }
             }
-        }
 
-        if (cursors.left.isDown) {
-            player.body.setVelocityX(-160);
-        }
-        else {
-            if (cursors.right.isDown) {
-                player.body.setVelocityX(160);
-            }   
-            else {
-                player.body.setVelocityX(0);
-            }
-        }
-
-        if (player.body.onFloor()) {
             if (cursors.left.isDown) {
-                playerLegs.anims.play('left', true); 
+                player.body.setVelocityX(-160);
             }
             else {
                 if (cursors.right.isDown) {
-                    playerLegs.anims.play('right', true);
+                    player.body.setVelocityX(160);
                 }   
                 else {
-                    playerLegs.anims.play('turn');
-                    playerTorso.anims.stop();
+                    player.body.setVelocityX(0);
                 }
             }
-        }else {
-            playerLegs.anims.stop();
-        }
 
-        if (cursors.up.isDown) {
             if (player.body.onFloor()) {
                 if (cursors.left.isDown) {
-                    playerTorso.anims.play('upLeftTorso', true);
-                    playerWeapons.side = 6;
-                }else if (cursors.right.isDown) {
-                    playerTorso.anims.play('upRightTorso', true);
-                    playerWeapons.side = 7;
-                }else {
-                    playerTorso.anims.play('upTorso', true);
-                    playerWeapons.side = 1;
+                    playerLegs.anims.play('left', true); 
+                }
+                else {
+                    if (cursors.right.isDown) {
+                        playerLegs.anims.play('right', true);
+                    }   
+                    else {
+                        playerLegs.anims.play('turn');
+                        playerTorso.anims.stop();
+                    }
                 }
             }else {
-                if (cursors.left.isDown) {
-                    playerTorso.anims.play('upLeftTorsoIdle', true);
-                    playerWeapons.side = 6;
-                }else if (cursors.right.isDown) {
-                    playerTorso.anims.play('upRightTorsoIdle', true);
-                    playerWeapons.side = 7;
-                }else {
-                    playerTorso.anims.play('upTorso', true);
-                    playerWeapons.side = 1;
-                }
+                playerLegs.anims.stop();
             }
-        }else {
-            if (cursors.down.isDown) {
+
+            if (cursors.up.isDown) {
                 if (player.body.onFloor()) {
                     if (cursors.left.isDown) {
-                        playerTorso.anims.play('downLeftTorso', true);
-                        playerWeapons.side = 2;
+                        playerTorso.anims.play('upLeftTorso', true);
+                        playerWeapons.side = 6;
                     }else if (cursors.right.isDown) {
-                        playerTorso.anims.play('downRightTorso', true);
-                        playerWeapons.side = 3;
+                        playerTorso.anims.play('upRightTorso', true);
+                        playerWeapons.side = 7;
                     }else {
-                        playerTorso.anims.play('downTorso', true);
-                        playerWeapons.side = 0;
+                        playerTorso.anims.play('upTorso', true);
+                        playerWeapons.side = 1;
                     }
                 }else {
                     if (cursors.left.isDown) {
-                        playerTorso.anims.play('downLeftTorsoIdle', true);
-                        playerWeapons.side = 2;
+                        playerTorso.anims.play('upLeftTorsoIdle', true);
+                        playerWeapons.side = 6;
                     }else if (cursors.right.isDown) {
-                        playerTorso.anims.play('downRightTorsoIdle', true);
-                        playerWeapons.side = 3;
+                        playerTorso.anims.play('upRightTorsoIdle', true);
+                        playerWeapons.side = 7;
                     }else {
-                        playerTorso.anims.play('downTorso', true);
-                        playerWeapons.side = 0;
+                        playerTorso.anims.play('upTorso', true);
+                        playerWeapons.side = 1;
                     }
                 }
-            }
-            else {
-                if (player.body.onFloor()) {
-                    if (cursors.left.isDown) {
-                        playerTorso.anims.play('leftTorso', true);
-                        playerWeapons.side = 4;
+            }else {
+                if (cursors.down.isDown) {
+                    if (player.body.onFloor()) {
+                        if (cursors.left.isDown) {
+                            playerTorso.anims.play('downLeftTorso', true);
+                            playerWeapons.side = 2;
+                        }else if (cursors.right.isDown) {
+                            playerTorso.anims.play('downRightTorso', true);
+                            playerWeapons.side = 3;
+                        }else {
+                            playerTorso.anims.play('downTorso', true);
+                            playerWeapons.side = 0;
+                        }
+                    }else {
+                        if (cursors.left.isDown) {
+                            playerTorso.anims.play('downLeftTorsoIdle', true);
+                            playerWeapons.side = 2;
+                        }else if (cursors.right.isDown) {
+                            playerTorso.anims.play('downRightTorsoIdle', true);
+                            playerWeapons.side = 3;
+                        }else {
+                            playerTorso.anims.play('downTorso', true);
+                            playerWeapons.side = 0;
+                        }
                     }
-                    else if (cursors.right.isDown) {
-                        playerTorso.anims.play('rightTorso', true);
-                        playerWeapons.side = 5;
-                    }
-                }else {
-                    if (cursors.left.isDown) {
-                        playerTorso.anims.play('leftTorsoIdle', true);
-                        playerWeapons.side = 4;
-                    }
-                    else if (cursors.right.isDown) {
-                        playerTorso.anims.play('rightTorsoIdle', true);
-                        playerWeapons.side = 5;
+                }
+                else {
+                    if (player.body.onFloor()) {
+                        if (cursors.left.isDown) {
+                            playerTorso.anims.play('leftTorso', true);
+                            playerWeapons.side = 4;
+                        }
+                        else if (cursors.right.isDown) {
+                            playerTorso.anims.play('rightTorso', true);
+                            playerWeapons.side = 5;
+                        }
+                    }else {
+                        if (cursors.left.isDown) {
+                            playerTorso.anims.play('leftTorsoIdle', true);
+                            playerWeapons.side = 4;
+                        }
+                        else if (cursors.right.isDown) {
+                            playerTorso.anims.play('rightTorsoIdle', true);
+                            playerWeapons.side = 5;
+                        }
                     }
                 }
             }
